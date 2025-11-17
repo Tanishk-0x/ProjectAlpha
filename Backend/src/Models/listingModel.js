fabeaace76bbeed3d4c45532944b307249e598a2
Tanishk-0x <tanishknamdev981@gmail.com>
2025-11-17 18:51:21 +0530

    Listing Model

---
+++ Backend/src/Models/listingModel.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose'); 
+
+const listingSchema = new mongoose.Schema({
+    title : {
+        type : String , 
+        required : true 
+    },
+    description : {
+        type : String , 
+        required : true
+    }, 
+
+    host : {
+        type : mongoose.Schema.Types.ObjectId , 
+        ref : "User" , 
+        required : true
+    }, 
+
+    image1 : {
+        type : String , 
+        required : true 
+    }, 
+    image2 : {
+        type : String , 
+        required : true
+    }, 
+    image3 : {
+        type : String , 
+        required : true
+    }, 
+
+    rent : {
+        type : Number , 
+        required : true
+    }, 
+    city : {
+        type : String , 
+        required : true
+    }, 
+    landmark : {
+        type : String , 
+        required : true
+    }, 
+    category : {
+        type : String , 
+        required : true
+    }, 
+
+    isBooked : {
+        type : Boolean , 
+        default : false 
+    }
+
+}, {timestamps:true}); 
+
+module.exports = mongoose.model("Listing" , listingSchema); 
