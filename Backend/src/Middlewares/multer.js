e512948b451d3690159b44b3ccffcab1d45912b5
Tanishk-0x <tanishknamdev981@gmail.com>
2025-11-18 17:03:18 +0530

    Multer

---
+++ Backend/src/Middlewares/multer.js
@@ -0,0 +1,17 @@
+const multer = require('multer'); 
+
+// Configure Storage
+const storage = multer.diskStorage({
+
+    destination : (req , file , cb) => {
+        cb( null , '../public'); 
+    }, 
+    filename : (req , file , cb) => {
+        cb( null , file.originalname);
+    }
+    
+}); 
+
+// Initialise Multer Middleware 
+const Upload = multer({storage}); 
+module.exports = Upload ; 
