547b10390745ff2d76a8b94ff85fb080cd3ffb81
Tanishk-0x <tanishknamdev981@gmail.com>
2026-01-10 18:20:16 +0530

    Route Fix

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -26,7 +26,7 @@ const App = () => {
         <Route path='/login' element={<Login/>} />
         <Route path='/listingpage1' element={ userData != null ? <ListingPage1/> : <Navigate to={'/'}/>}/>
         <Route path='/listingpage2' element={ userData != null ? <ListingPage2/> : <Navigate to={'/'}/>}/>
-        <Route path='/listingpage3' element={<ListingPage3/>}/>
+        <Route path='/listingpage3' element={ userData != null ? <ListingPage3/> : <Navigate to={'/'}/>}/>
         <Route path='/mylisting' element={ userData != null ? <MyListing/> : <Navigate to={'/'}/>}/>
         <Route path='/viewcard' element={userData != null ? <ViewCard/> : <Navigate to={'/'}/>}/>
         <Route path='/mybooking' element={userData != null ? <MyBooking/> : <Navigate to={'/'}/>}/>
